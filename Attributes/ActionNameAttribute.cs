fcd98002df684276894a3385067468b8d735c55a
pancarkedi <pancarkedi@hotmail.com>
2016-10-24 13:25:26 +0300

    Http request naming attributes

---
+++ Attributes/ActionNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace likeselfhosting.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ActionNameAttribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public ActionNameAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Attributes/ComponentNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace likeselfhosting.Attributes
+{
+    [AttributeUsage(AttributeTargets.Interface)]
+    public class ComponentNameAttribute : Attribute
+    {
+        public string Name { get; private set; }
+
+        public ComponentNameAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
